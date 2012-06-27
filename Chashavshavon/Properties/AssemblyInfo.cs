c46c756050ce28670e1dd3dbc26e21f5fe2bb06c
C.B. Sommers <cb@compute.co.il>
2012-06-27 12:49:02 +0300

    Chashavshavon_Installer_64bit.exe Version 3.0.4

--- Chashavshavon/Properties/AssemblyInfo.cs
+++ Chashavshavon/Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Resources;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.0.2")]
-[assembly: AssemblyFileVersion("3.0.2")]
+[assembly: AssemblyVersion("3.0.4")]
+[assembly: AssemblyFileVersion("3.0.4")]
 [assembly: NeutralResourcesLanguageAttribute("he")]
