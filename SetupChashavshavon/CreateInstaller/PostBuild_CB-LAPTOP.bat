cf7f520e88ec00e5f1785150c13eae3ad39ce64c
C.B. Sommers <cb@compute.co.il>
2012-07-17 18:31:38 +0300

    Changed Post Build text files to batch files named by %COMPUTERNAME%  - to automatically run the correct batch file.

---
+++ SetupChashavshavon/CreateInstaller/PostBuild_CB-LAPTOP.bat
@@ -0,0 +1,6 @@
+"C:\Program Files\7-Zip\7z.exe " a "Chashavshavon_Installer.zip" "%~1%~2\*"
+"D:\Program Files\ZipGenius 6\zg.exe " -sfx "Chashavshavon_Installer.zip" A1 B1 O0 E0 L0 C"Setup.exe" D"%temp%" U"ChashInstall" H2 I"scroll.ico" S0 T"Extracting Chashavshavon installation files..."
+DEL "Chashavshavon_Installer.zip"
+MOVE "Chashavshavon_Installer.exe" "D:\Repositories\Compute\WebSite\Products\Chashavshavon\Chashavshavon_Installer.exe"
+"C:\Program Files\7-Zip\7z.exe " a "D:\Repositories\Compute\WebSite\Products\Chashavshavon\Chashavshavon_Installer.zip" "D:\Repositories\Compute\WebSite\Products\Chashavshavon\Chashavshavon_Installer.exe"
+"SetVersionApp.exe " "D:\Repositories\Chashavshavon\Chashavshavon\bin\%~2\Chashavshavon.exe" "D:\Repositories\Compute\WebSite\Products\Chashavshavon"
---
+++ SetupChashavshavon/CreateInstaller/PostBuild_CBS-PC.bat
@@ -0,0 +1,6 @@
+"C:\Program Files\7-Zip\7z.exe " a "%~2Chashavshavon_Installer.zip" "%~1%~2\*"
+"C:\Program Files\ZipGenius 6\zg.exe" -sfx "%~2Chashavshavon_Installer.zip" A1 B1 O0 E0 L0 C"Setup.exe" D"%temp%" U"ChashInstall" H2 I"%~2scroll.ico" S0 T"Extracting Chashavshavon installation files..."
+DEL "%~2Chashavshavon_Installer.zip"
+MOVE "%~2Chashavshavon_Installer.exe" "D:\Insync\cb@compute.co.il\Repositories\Compute\WebSite\Products\Chashavshavon\Chashavshavon_Installer.exe"
+"C:\Program Files\7-Zip\7z.exe " a "D:\Insync\cb@compute.co.il\Repositories\Compute\WebSite\Products\Chashavshavon\Chashavshavon_Installer.zip" "D:\Insync\cb@compute.co.il\Repositories\Compute\WebSite\Products\Chashavshavon\Chashavshavon_Installer.exe"
+"%~2SetVersionApp.exe " "D:\Insync\cb@compute.co.il\Repositories\Chashavshavon\Chashavshavon\bin\%~2\Chashavshavon.exe" "D:\Insync\cb@compute.co.il\Repositories\Compute\WebSite\Products\Chashavshavon"
