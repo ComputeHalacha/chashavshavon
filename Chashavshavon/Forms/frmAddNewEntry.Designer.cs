193d4219ee2bc8c189dc471c450a1cf855899613
Chaim B. Sommers <cb@compute.co.il>
2020-04-26 21:50:09 +0300

    Small GUI changes.

--- Chashavshavon/Forms/frmAddNewEntry.Designer.cs
+++ Chashavshavon/Forms/frmAddNewEntry.Designer.cs
@@ -45,13 +45,13 @@
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.btnToday = new System.Windows.Forms.Button();
+            this.lblCaption = new System.Windows.Forms.Label();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.label6 = new System.Windows.Forms.Label();
             this.webBrowser1 = new System.Windows.Forms.WebBrowser();
             this.panel4 = new System.Windows.Forms.Panel();
             this.btnClose = new System.Windows.Forms.Button();
-            this.lblCaption = new System.Windows.Forms.Label();
-            this.btnToday = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
@@ -66,7 +66,7 @@
             this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.Color.Transparent;
-            this.label3.Location = new System.Drawing.Point(1023, 12);
+            this.label3.Location = new System.Drawing.Point(893, 12);
             this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(26, 18);
@@ -78,7 +78,7 @@
             this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label4.AutoSize = true;
             this.label4.BackColor = System.Drawing.Color.Transparent;
-            this.label4.Location = new System.Drawing.Point(577, 68);
+            this.label4.Location = new System.Drawing.Point(484, 46);
             this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(52, 18);
@@ -96,7 +96,7 @@
             this.cmbMonth.Font = new System.Drawing.Font("Tahoma", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
             this.cmbMonth.ForeColor = System.Drawing.Color.SaddleBrown;
             this.cmbMonth.FormattingEnabled = true;
-            this.cmbMonth.Location = new System.Drawing.Point(910, 35);
+            this.cmbMonth.Location = new System.Drawing.Point(780, 35);
             this.cmbMonth.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.cmbMonth.Name = "cmbMonth";
             this.cmbMonth.Size = new System.Drawing.Size(72, 24);
@@ -115,7 +115,7 @@
             this.cmbDay.Font = new System.Drawing.Font("Tahoma", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
             this.cmbDay.ForeColor = System.Drawing.Color.SaddleBrown;
             this.cmbDay.FormattingEnabled = true;
-            this.cmbDay.Location = new System.Drawing.Point(986, 35);
+            this.cmbDay.Location = new System.Drawing.Point(856, 35);
             this.cmbDay.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.cmbDay.Name = "cmbDay";
             this.cmbDay.Size = new System.Drawing.Size(58, 24);
@@ -133,7 +133,7 @@
             this.cmbYear.Font = new System.Drawing.Font("Tahoma", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
             this.cmbYear.ForeColor = System.Drawing.Color.SaddleBrown;
             this.cmbYear.FormattingEnabled = true;
-            this.cmbYear.Location = new System.Drawing.Point(837, 35);
+            this.cmbYear.Location = new System.Drawing.Point(707, 35);
             this.cmbYear.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.cmbYear.Name = "cmbYear";
             this.cmbYear.Size = new System.Drawing.Size(66, 24);
@@ -161,7 +161,7 @@
             this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Location = new System.Drawing.Point(878, 12);
+            this.label1.Location = new System.Drawing.Point(748, 12);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(36, 18);
@@ -189,7 +189,7 @@
             this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.Transparent;
-            this.label2.Location = new System.Drawing.Point(951, 12);
+            this.label2.Location = new System.Drawing.Point(821, 12);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(43, 18);
@@ -202,11 +202,11 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.txtNotes.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txtNotes.ForeColor = System.Drawing.Color.SaddleBrown;
-            this.txtNotes.Location = new System.Drawing.Point(329, 89);
+            this.txtNotes.Location = new System.Drawing.Point(277, 67);
             this.txtNotes.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.txtNotes.Multiline = true;
             this.txtNotes.Name = "txtNotes";
-            this.txtNotes.Size = new System.Drawing.Size(300, 39);
+            this.txtNotes.Size = new System.Drawing.Size(256, 61);
             this.txtNotes.TabIndex = 21;
             // 
             // btnEnter
@@ -245,7 +245,7 @@
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.groupBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.groupBox1.Size = new System.Drawing.Size(646, 146);
+            this.groupBox1.Size = new System.Drawing.Size(550, 146);
             this.groupBox1.TabIndex = 26;
             this.groupBox1.TabStop = false;
             // 
@@ -256,7 +256,7 @@
             this.label5.BackColor = System.Drawing.Color.Transparent;
             this.label5.Font = new System.Drawing.Font("Tahoma", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
             this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.label5.Location = new System.Drawing.Point(420, 10);
+            this.label5.Location = new System.Drawing.Point(324, 10);
             this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(224, 27);
@@ -272,7 +272,7 @@
             this.groupBox2.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Padding = new System.Windows.Forms.Padding(2, 3, 2, 3);
-            this.groupBox2.Size = new System.Drawing.Size(1014, 107);
+            this.groupBox2.Size = new System.Drawing.Size(884, 107);
             this.groupBox2.TabIndex = 27;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "התראות היום";
@@ -283,7 +283,8 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.tableLayoutPanel1.AutoScroll = true;
-            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.tableLayoutPanel1.ColumnCount = 1;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.Font = new System.Drawing.Font("Tahoma", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
@@ -294,7 +295,7 @@
             this.tableLayoutPanel1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.tableLayoutPanel1.RowCount = 1;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(642, 333);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(546, 333);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // panel2
@@ -313,8 +314,47 @@
             this.panel2.Location = new System.Drawing.Point(0, 0);
             this.panel2.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1052, 72);
+            this.panel2.Size = new System.Drawing.Size(922, 72);
             this.panel2.TabIndex = 30;
+            // 
+            // btnToday
+            // 
+            this.btnToday.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnToday.BackColor = System.Drawing.Color.Transparent;
+            this.btnToday.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnToday.BackgroundImage")));
+            this.btnToday.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnToday.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnToday.FlatAppearance.BorderSize = 0;
+            this.btnToday.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnToday.Font = new System.Drawing.Font("Narkisim", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.btnToday.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(205)))), ((int)(((byte)(208)))), ((int)(((byte)(210)))));
+            this.btnToday.Image = global::Chashavshavon.Properties.Resources.down;
+            this.btnToday.Location = new System.Drawing.Point(602, 28);
+            this.btnToday.Name = "btnToday";
+            this.btnToday.Size = new System.Drawing.Size(89, 31);
+            this.btnToday.TabIndex = 37;
+            this.btnToday.Text = "היום";
+            this.btnToday.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnToday.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnToday.UseVisualStyleBackColor = false;
+            this.btnToday.Click += new System.EventHandler(this.btnToday_Click);
+            // 
+            // lblCaption
+            // 
+            this.lblCaption.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblCaption.AutoEllipsis = true;
+            this.lblCaption.BackColor = System.Drawing.Color.Transparent;
+            this.lblCaption.Font = new System.Drawing.Font("Tahoma", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.lblCaption.ForeColor = System.Drawing.Color.SteelBlue;
+            this.lblCaption.Location = new System.Drawing.Point(2, 0);
+            this.lblCaption.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblCaption.Name = "lblCaption";
+            this.lblCaption.Size = new System.Drawing.Size(571, 59);
+            this.lblCaption.TabIndex = 28;
+            this.lblCaption.Text = "כותרת";
+            this.lblCaption.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // splitContainer1
             // 
@@ -335,7 +375,7 @@
             this.splitContainer1.Panel2.Controls.Add(this.webBrowser1);
             this.splitContainer1.Panel2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             this.splitContainer1.Size = new System.Drawing.Size(1271, 538);
-            this.splitContainer1.SplitterDistance = 646;
+            this.splitContainer1.SplitterDistance = 550;
             this.splitContainer1.TabIndex = 32;
             // 
             // label6
@@ -345,7 +385,7 @@
             this.label6.BackColor = System.Drawing.Color.Transparent;
             this.label6.Font = new System.Drawing.Font("Tahoma", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
             this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(80)))), ((int)(((byte)(0)))));
-            this.label6.Location = new System.Drawing.Point(385, 10);
+            this.label6.Location = new System.Drawing.Point(289, 10);
             this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(259, 27);
@@ -361,7 +401,7 @@
             this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
             this.webBrowser1.Name = "webBrowser1";
             this.webBrowser1.ScriptErrorsSuppressed = true;
-            this.webBrowser1.Size = new System.Drawing.Size(621, 538);
+            this.webBrowser1.Size = new System.Drawing.Size(717, 538);
             this.webBrowser1.TabIndex = 2;
             this.webBrowser1.WebBrowserShortcutsEnabled = false;
             // 
@@ -374,7 +414,7 @@
             this.panel4.Location = new System.Drawing.Point(0, 614);
             this.panel4.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(1052, 56);
+            this.panel4.Size = new System.Drawing.Size(922, 56);
             this.panel4.TabIndex = 33;
             // 
             // btnClose
@@ -396,51 +436,12 @@
             this.btnClose.Text = "סגור";
             this.btnClose.UseVisualStyleBackColor = false;
             // 
-            // lblCaption
-            // 
-            this.lblCaption.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.lblCaption.AutoEllipsis = true;
-            this.lblCaption.BackColor = System.Drawing.Color.Transparent;
-            this.lblCaption.Font = new System.Drawing.Font("Tahoma", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
-            this.lblCaption.ForeColor = System.Drawing.Color.SteelBlue;
-            this.lblCaption.Location = new System.Drawing.Point(2, 0);
-            this.lblCaption.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.lblCaption.Name = "lblCaption";
-            this.lblCaption.Size = new System.Drawing.Size(701, 59);
-            this.lblCaption.TabIndex = 28;
-            this.lblCaption.Text = "כותרת";
-            this.lblCaption.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            // 
-            // btnToday
-            // 
-            this.btnToday.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnToday.BackColor = System.Drawing.Color.Transparent;
-            this.btnToday.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnToday.BackgroundImage")));
-            this.btnToday.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.btnToday.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnToday.FlatAppearance.BorderSize = 0;
-            this.btnToday.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnToday.Font = new System.Drawing.Font("Narkisim", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
-            this.btnToday.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(205)))), ((int)(((byte)(208)))), ((int)(((byte)(210)))));
-            this.btnToday.Image = global::Chashavshavon.Properties.Resources.down;
-            this.btnToday.Location = new System.Drawing.Point(732, 28);
-            this.btnToday.Name = "btnToday";
-            this.btnToday.Size = new System.Drawing.Size(89, 31);
-            this.btnToday.TabIndex = 37;
-            this.btnToday.Text = "היום";
-            this.btnToday.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.btnToday.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
-            this.btnToday.UseVisualStyleBackColor = false;
-            this.btnToday.Click += new System.EventHandler(this.btnToday_Click);
-            // 
             // frmAddNewEntry
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1052, 670);
+            this.ClientSize = new System.Drawing.Size(922, 670);
             this.Controls.Add(this.panel4);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.splitContainer1);
