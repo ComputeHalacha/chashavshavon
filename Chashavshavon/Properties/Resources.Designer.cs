dcd2eba43d3a7a29b39f1bb778cb7d42973a831a
Chaim B. Sommers <cb@compute.co.il>
2020-04-29 18:53:21 +0300

    Fixed up https://remotechashavshavon.appspot.com code.

--- Chashavshavon/Properties/Resources.Designer.cs
+++ Chashavshavon/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Chashavshavon.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -71,7 +71,7 @@ namespace Chashavshavon.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to http://chashavshavon.appspot.com.
+        ///   Looks up a localized string similar to https://remotechashavshavon.appspot.com.
         /// </summary>
         internal static string AppURL {
             get {
